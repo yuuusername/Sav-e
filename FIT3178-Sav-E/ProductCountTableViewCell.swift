832911810b8cd082d8de01190c7da6d1b845fec6
Dylan Hor <dhor0003@student.monash.edu>
2022-05-02 15:41:48 +1000

    UI updates to prepare to support All Products view controller

---
+++ FIT3178-Sav-E/ProductCountTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProductCountTableViewCell.swift
+//  FIT3178-Sav-E
+//
+//  Created by Dylan Hor on 2/5/2022.
+//
+
+import UIKit
+
+class ProductCountTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var totalLable: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
